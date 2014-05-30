ff3cdaab89891cc6cf80878f66d4ab18f7f29cfa
WellHydrated <michaelfry2002@gmail.com>
2014-05-30 13:39:09 +1200

    Moved logic into directions, fixes
    Fixed bugs in bearing

--- src/com/navidroid/model/directions/Direction.java
+++ src/com/navidroid/model/directions/Direction.java
@@ -1,25 +1,11 @@
 package com.navidroid.model.directions;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.navidroid.model.LatLng;
-import com.navidroid.model.util.LatLngUtil;
 import com.navidroid.model.util.StringUtil;
 
 public class Direction {
-	
-	private final static int MIN_DIST_FOR_HEADING_METERS = 5;
-	private final static int MIN_U_TURN_ANGLE = 175;
-	private final static int MIN_SHARP_TURN_ANGLE = 150;
-	private final static int MIN_SHARP_RIGHT_TURN_ANGLE = MIN_SHARP_TURN_ANGLE;
-	private final static int MIN_SHARP_LEFT_TURN_ANGLE = -MIN_SHARP_TURN_ANGLE;
-	private final static int MIN_TURN_ANGLE = 45;
-	private final static int MIN_RIGHT_TURN_ANGLE = MIN_TURN_ANGLE;
-	private final static int MIN_LEFT_TURN_ANGLE = -MIN_TURN_ANGLE;
-	private final static int MIN_VEER_MOVEMENT_ANGLE = 10;
-	private final static int MIN_RIGHT_VEER_ANGLE = MIN_VEER_MOVEMENT_ANGLE;
-	private final static int MIN_LEFT_VEER_ANGLE = -MIN_VEER_MOVEMENT_ANGLE;
 	
 	private List<LatLng> path;
 	private int timeSeconds;
@@ -30,90 +16,19 @@ public class Direction {
 	private String current;
 	private String target;
 	private Movement movement;
-	
-	public Direction(List<LatLng> path, int timeSeconds, int distanceMeters, String description, String current, String target, Direction nextDirection) {
-		this(path, timeSeconds, distanceMeters, description, current, target, calculateMovement(path, nextDirection.getPath()));
-	}
-	
-	public Direction(List<LatLng> path, int timeSeconds, int distanceMeters, String description, String current, String target, Movement movement) {
+
+	public Direction(List<LatLng> path, int timeSeconds, int distanceMeters, String description, String current, String target) {
 		this.path = path;
 		this.timeSeconds = timeSeconds;
 		this.distanceMeters = distanceMeters;
 		this.description = description;
 		this.current = current;
 		this.target = target;
-		this.movement = movement;
-		createMovementDescription();
-		createShortDescription();
 	}
 	
-	private static Movement calculateMovement(List<LatLng> currentPath, List<LatLng> nextPath) {
-		if (currentPath.size() < 2 || nextPath.size() < 2) {
-			return Movement.UNKNOWN;
-		}
-		
-		double movementAngle = calculateAngleAtMovement(currentPath, nextPath);
-		if (movementAngle > 180) {
-			return Movement.UNKNOWN;
-		} else if (movementAngle > MIN_U_TURN_ANGLE) {
-			return Movement.U_TURN;
-		} else if (movementAngle > MIN_SHARP_RIGHT_TURN_ANGLE) {
-			return Movement.TURN_RIGHT_SHARP;
-		} else if (movementAngle > MIN_RIGHT_TURN_ANGLE) {
-			return Movement.TURN_RIGHT;
-		} else if (movementAngle > MIN_RIGHT_VEER_ANGLE) {
-			return Movement.VEER_RIGHT;
-		} else if (movementAngle > MIN_LEFT_VEER_ANGLE) {
-			return Movement.CONTINUE;
-		} else if (movementAngle > MIN_LEFT_TURN_ANGLE) {
-			return Movement.VEER_LEFT;
-		} else if (movementAngle > MIN_SHARP_LEFT_TURN_ANGLE) {
-			return Movement.TURN_LEFT;
-		} else if (movementAngle > -MIN_U_TURN_ANGLE) {
-			return Movement.TURN_LEFT_SHARP;
-		} else if (movementAngle >= -180) {
-			return Movement.U_TURN;
-		} else {
-			return Movement.UNKNOWN;
-		}
-	}
-	
-	private static double calculateAngleAtMovement(List<LatLng> currentPath, List<LatLng> nextPath) {
-		double currentPathFinalHeading = calculateHeadingAtPathEnd(currentPath);
-		double nextPathInitialHeading = calculateHeadingAtPathStart(currentPath);
-		return nextPathInitialHeading - currentPathFinalHeading;
-	}
-	
-	private static double calculateHeadingAtPathEnd(List<LatLng> path) {
-		return calculateHeadingOfPath(getRelevantPoints(path, false));
-	}
-	
-	private static double calculateHeadingAtPathStart(List<LatLng> path) {
-		return calculateHeadingOfPath(getRelevantPoints(path, true));
-	}
-	
-	private static List<LatLng> getRelevantPoints(List<LatLng> path, boolean fromStart) {
-		ArrayList<LatLng> points = new ArrayList<LatLng>();
-		int pathSize = path.size();
-		
-		if (pathSize > 0) {
-			int i = fromStart ? 0 : path.size();
-			int iterationDirection = fromStart ? 1 : -1;
-			int travelledDistance = 0;
-			LatLng currentLocation = path.get(i);
-			while (i >= 0 && i < path.size() && travelledDistance < MIN_DIST_FOR_HEADING_METERS) {
-				LatLng nextLocation = path.get(i);
-				travelledDistance += LatLngUtil.distanceInMeters(currentLocation, nextLocation);
-				currentLocation = nextLocation;
-				i += iterationDirection;
-			}
-		}
-		
-		return points;
-	}
-	
-	private static double calculateHeadingOfPath(List<LatLng> path) {
-		return LatLngUtil.initialBearing(path.get(0), path.get(path.size() - 1));
+	public Direction(List<LatLng> path, int timeSeconds, int distanceMeters, String description, String current, String target, Movement movement) {
+		this(path, timeSeconds, distanceMeters, description, current, target);
+		setMovement(movement);
 	}
 	
 	public List<LatLng> getPath() {
@@ -150,6 +65,13 @@ public class Direction {
 	
 	public Movement getMovement() {
 		return movement;
+	}
+	
+	/*internal*/ void setMovement(Movement movement) {
+		assert movement == null;
+		this.movement = movement;
+		createMovementDescription();
+		createShortDescription();
 	}
 	
 	private void createMovementDescription() {
--- src/com/navidroid/model/directions/Directions.java
+++ src/com/navidroid/model/directions/Directions.java
@@ -1,12 +1,25 @@
 package com.navidroid.model.directions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.navidroid.model.LatLng;
 import com.navidroid.model.util.LatLngUtil;
 
 public class Directions {
+	
+	private final static int MIN_DIST_FOR_Bearing_METERS = 5;
+	private final static int MIN_U_TURN_ANGLE = 175;
+	private final static int MIN_SHARP_TURN_ANGLE = 150;
+	private final static int MIN_SHARP_RIGHT_TURN_ANGLE = MIN_SHARP_TURN_ANGLE;
+	private final static int MIN_SHARP_LEFT_TURN_ANGLE = -MIN_SHARP_TURN_ANGLE;
+	private final static int MIN_TURN_ANGLE = 45;
+	private final static int MIN_RIGHT_TURN_ANGLE = MIN_TURN_ANGLE;
+	private final static int MIN_LEFT_TURN_ANGLE = -MIN_TURN_ANGLE;
+	private final static int MIN_VEER_MOVEMENT_ANGLE = 10;
+	private final static int MIN_RIGHT_VEER_ANGLE = MIN_VEER_MOVEMENT_ANGLE;
+	private final static int MIN_LEFT_VEER_ANGLE = -MIN_VEER_MOVEMENT_ANGLE;
 	
 	private List<Direction> directions;
 	private ArrayList<Point> path;
@@ -26,10 +39,11 @@ public class Directions {
 		this.originAddress = originAddress;
 		this.destinationAddress = destinationAddress;
 		this.directions = directions;
-		validateDirections();
 		createPath();
 		indexPath();
+		calculateMovements();
 		createLatLngPath();
+		validateDirections();
 	}
 	
 	private void validateDirections() throws Exception {
@@ -117,6 +131,93 @@ public class Directions {
 			currentIndex++;
 			lastPoint = currentPoint;
 		} while ((currentPoint = currentPoint.next) != null);
+	}
+	
+	private void calculateMovements() {
+		directions.get(0).setMovement(Movement.DEPARTURE);
+		int numberOfDirections = directions.size();
+		for (int i = 1; i < numberOfDirections - 1; i++) {
+			Direction currentDirection = directions.get(i);
+			if (currentDirection.getMovement() == null) {
+				Movement currentMovement = calculateMovement(currentDirection.getPath(), directions.get(i + 1).getPath());
+				currentDirection.setMovement(currentMovement);
+			}
+		}
+		directions.get(numberOfDirections - 1).setMovement(Movement.ARRIVAL);
+	}
+	
+	private Movement calculateMovement(List<LatLng> currentPath, List<LatLng> nextPath) {
+		if (currentPath.size() < 2 || nextPath.size() < 2) {
+			return Movement.UNKNOWN;
+		}
+		
+		double movementAngle = calculateAngleAtMovement(currentPath, nextPath);
+		if (movementAngle > 180) {
+			return Movement.UNKNOWN;
+		} else if (movementAngle > MIN_U_TURN_ANGLE) {
+			return Movement.U_TURN;
+		} else if (movementAngle > MIN_SHARP_RIGHT_TURN_ANGLE) {
+			return Movement.TURN_RIGHT_SHARP;
+		} else if (movementAngle > MIN_RIGHT_TURN_ANGLE) {
+			return Movement.TURN_RIGHT;
+		} else if (movementAngle > MIN_RIGHT_VEER_ANGLE) {
+			return Movement.VEER_RIGHT;
+		} else if (movementAngle > MIN_LEFT_VEER_ANGLE) {
+			return Movement.CONTINUE;
+		} else if (movementAngle > MIN_LEFT_TURN_ANGLE) {
+			return Movement.VEER_LEFT;
+		} else if (movementAngle > MIN_SHARP_LEFT_TURN_ANGLE) {
+			return Movement.TURN_LEFT;
+		} else if (movementAngle > -MIN_U_TURN_ANGLE) {
+			return Movement.TURN_LEFT_SHARP;
+		} else if (movementAngle >= -180) {
+			return Movement.U_TURN;
+		} else {
+			return Movement.UNKNOWN;
+		}
+	}
+	
+	private double calculateAngleAtMovement(List<LatLng> currentPath, List<LatLng> nextPath) {
+		double currentPathFinalBearing = calculateBearingAtPathEnd(currentPath);
+		double nextPathInitialBearing = calculateBearingAtPathStart(nextPath);
+		return 180 - LatLngUtil.normalizeBearing(nextPathInitialBearing - currentPathFinalBearing);
+	}
+	
+	private double calculateBearingAtPathEnd(List<LatLng> path) {
+		return calculateBearingOfPath(getRelevantPoints(path, false));
+	}
+	
+	private double calculateBearingAtPathStart(List<LatLng> path) {
+		return calculateBearingOfPath(getRelevantPoints(path, true));
+	}
+	
+	private List<LatLng> getRelevantPoints(List<LatLng> path, boolean fromStart) {
+		ArrayList<LatLng> points = new ArrayList<LatLng>();
+		int pathSize = path.size();
+		
+		if (pathSize > 0) {
+			int i = fromStart ? 0 : path.size() - 1;
+			int iterationDirection = fromStart ? 1 : -1;
+			int travelledDistance = 0;
+			LatLng currentLocation = path.get(i);
+			while (i >= 0 && i < path.size() && travelledDistance < MIN_DIST_FOR_Bearing_METERS) {
+				LatLng nextLocation = path.get(i);
+				travelledDistance += LatLngUtil.distanceInMeters(currentLocation, nextLocation);
+				currentLocation = nextLocation;
+				points.add(currentLocation);
+				i += iterationDirection;
+			}
+		}
+		
+		if (!fromStart) {
+			Collections.reverse(points);
+		}
+		
+		return points;
+	}
+	
+	private double calculateBearingOfPath(List<LatLng> path) {
+		return LatLngUtil.initialBearing(path.get(0), path.get(path.size() - 1));
 	}
 	
 	private void createLatLngPath() {
